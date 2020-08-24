793493ca9fae6084ca07b86197ce61cbd2735a53
Abylai03 <61981188+Abylai03@users.noreply.github.com>
2020-08-25 01:32:39 +0600

    modal window

--- scripts/main.js
+++ scripts/main.js
@@ -23,7 +23,7 @@ const salonform = document.querySelector(".salon");
 const masterform = document.querySelector(".master");
 
 salons.addEventListener("click", () => {
-salons.classList.add("isactive");
+  salons.classList.add("isactive");
   salonform.classList.remove("hide");
   masters.classList.remove("isactive");
   masterform.classList.add("hide");
@@ -36,91 +36,99 @@ masters.addEventListener("click", () => {
   salonform.classList.add("hide");
 });
 window.onscroll = function showHeader() {
-  var menu = document.querySelector('.menu');
+  var menu = document.querySelector(".menu");
   if (window.pageYOffset > 200) {
-    menu.classList.add('fixed');
+    menu.classList.add("fixed");
+  } else if (menu.classList.contains("fixed")) {
+    menu.classList.remove("fixed");
   }
-  else if(menu.classList.contains('fixed')){
-    menu.classList.remove('fixed');
-  }
+};
+function slowScroll(id) {
+  var offset = 0;
+  $("html, body").animate(
+    {
+      scrollTop: $(id).offset().top - offset,
+    },
+    500
+  );
+  return false;
 }
-function slowScroll (id) {
-    var offset = 0;
-    $('html, body').animate ({
-      scrollTop: $(id).offset ().top - offset
-    }, 500);
-    return false;
-  };
 
-  function countdown(dateEnd) {
-    var timer, days, hours, minutes, seconds;
+function countdown(dateEnd) {
+  var timer, days, hours, minutes, seconds;
 
-    dateEnd = new Date(dateEnd);
-    dateEnd = dateEnd.getTime();
+  dateEnd = new Date(dateEnd);
+  dateEnd = dateEnd.getTime();
 
-    if (isNaN(dateEnd)) {
+  if (isNaN(dateEnd)) {
+    return;
+  }
+
+  timer = setInterval(calculate, 1000);
+
+  function calculate() {
+    var dateStart = new Date();
+    var dateStart = new Date(
+      dateStart.getUTCFullYear(),
+      dateStart.getUTCMonth(),
+      dateStart.getUTCDate(),
+      dateStart.getUTCHours(),
+      dateStart.getUTCMinutes(),
+      dateStart.getUTCSeconds()
+    );
+    var timeRemaining = parseInt((dateEnd - dateStart.getTime()) / 1000);
+
+    if (timeRemaining >= 0) {
+      days = parseInt(timeRemaining / 86400);
+      timeRemaining = timeRemaining % 86400;
+      hours = parseInt(timeRemaining / 3600);
+      timeRemaining = timeRemaining % 3600;
+      minutes = parseInt(timeRemaining / 60);
+      timeRemaining = timeRemaining % 60;
+      seconds = parseInt(timeRemaining);
+
+      document.getElementById("days").innerHTML = parseInt(days, 10);
+      document.getElementById("hours").innerHTML = ("0" + hours).slice(-2);
+      document.getElementById("minutes").innerHTML = ("0" + minutes).slice(-2);
+      document.getElementById("seconds").innerHTML = ("0" + seconds).slice(-2);
+    } else {
       return;
     }
-
-    timer = setInterval(calculate, 1000);
-
-    function calculate() {
-      var dateStart = new Date();
-      var dateStart = new Date(
-        dateStart.getUTCFullYear(),
-        dateStart.getUTCMonth(),
-        dateStart.getUTCDate(),
-        dateStart.getUTCHours(),
-        dateStart.getUTCMinutes(),
-        dateStart.getUTCSeconds()
-      );
-      var timeRemaining = parseInt((dateEnd - dateStart.getTime()) / 1000);
-
-      if (timeRemaining >= 0) {
-        days = parseInt(timeRemaining / 86400);
-        timeRemaining = timeRemaining % 86400;
-        hours = parseInt(timeRemaining / 3600);
-        timeRemaining = timeRemaining % 3600;
-        minutes = parseInt(timeRemaining / 60);
-        timeRemaining = timeRemaining % 60;
-        seconds = parseInt(timeRemaining);
-
-        document.getElementById("days").innerHTML = parseInt(days, 10);
-        document.getElementById("hours").innerHTML = ("0" + hours).slice(-2);
-        document.getElementById("minutes").innerHTML = ("0" + minutes).slice(-2);
-        document.getElementById("seconds").innerHTML = ("0" + seconds).slice(-2);
-      } else {
-        return;
-      }
-    }
-
-    function display(days, hours, minutes, seconds) {}
   }
 
-  countdown("11/01/2020 00:00:00 AM");
-  var gamb = document.querySelector('.gamb');
-var minMenu = document.querySelector('.min-menu');
-  var arrow = document.querySelector('.min-menu img');
-  gamb.onclick = function () {
-      minMenu.classList.add('show');
-      gamb.classList.add('delete');
-      arrow.classList.add('show');
-  }
-  arrow.onclick = function () {
-      minMenu.classList.remove('show');
-      gamb.classList.remove('delete');
-      arrow.classList.remove('show');
-  };
+  function display(days, hours, minutes, seconds) {}
+}
 
-  
-  
- const registerBtn = document.querySelector(".r-btn");
+countdown("11/01/2020 00:00:00 AM");
+var gamb = document.querySelector(".gamb");
+var minMenu = document.querySelector(".min-menu");
+var arrow = document.querySelector(".min-menu img");
+gamb.onclick = function () {
+  minMenu.classList.add("show");
+  gamb.classList.add("delete");
+  arrow.classList.add("show");
+};
+arrow.onclick = function () {
+  minMenu.classList.remove("show");
+  gamb.classList.remove("delete");
+  arrow.classList.remove("show");
+};
+
+const registerBtn = document.querySelector(".r-btn");
 const userSignup = document.querySelector(".user-signup1");
 
-registerBtn.addEventListener("click", () => {
+const master = document.querySelector(".master");
+const salon = document.querySelector(".salon");
+
+master.addEventListener("submit", (e) => {
+  e.preventDefault();
   userSignup.classList.remove("hide");
 });
 
+salon.addEventListener("submit", (e) => {
+  e.preventDefault();
+  userSignup.classList.remove("hide");
+});
 userSignup.addEventListener("click", (event) => {
   const target = event.target;
 
