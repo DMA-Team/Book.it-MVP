82f6aa9b6be2f084107dc6d3393d34df40906418
Abylai03 <bite2099@gmail.com>
2020-08-28 10:38:29 +0600

    registration and aos

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,39 +1,139 @@
+const database = firebase.firestore();
+database.settings({ timestampsInSnapshots: true });
+
+
 const form = document.getElementById("add-salon");
+const masterForm = document.querySelector(".master");
+const phoneform = document.getElementById("phone_form");
+const userSignup = document.querySelector(".user-signup1");
+
 form.addEventListener("submit", (e) => {
-  var notifications = document.getElementById("notification1");
-  if (notifications.checked) {
-    notifications.value = true;
-  } else {
-    notifications.value = false;
-  }
   e.preventDefault();
-  db.collection("salons").add({
-    full_name: form.full_name.value,
-    email: form.email.value,
-    salon_name: form.salonname.value,
-    phone_number: form.phone_number.value,
-    notifications: form.notifications.value,
-    password: form.password.value
-  });
-  form.reset();
+  var sEmail = document.querySelector(".s-email").value;
+  let x = false;
+  database.collection("salons")
+    .get()
+    .then((snapshot) => {
+      snapshot.docs.forEach((doc) => {
+        var firestoreEmail = doc.data().email;
+        if (firestoreEmail == sEmail.value) {
+          x = true;
+        }
+      });
+    });
+  if (x == true) {
+    alert("Пользователь с такой почтой уже существует");
+  } else {
+    userSignup.classList.remove("hide");
+  }
+});
+masterForm.addEventListener("submit", (o) => {
+  var mEmail = document.querySelector(".m-email").value;
+  let x = false;
+  database.collection("masters")
+    .get()
+    .then((snapshot) => {
+      snapshot.docs.forEach((doc) => {
+        var firestoreMEmail = doc.data().email;
+        if (firestoreMEmail.value == mEmail.value) {
+          x = true;
+        }
+      });
+    });
+  if (x == true) {
+    alert("Пользователь с такой почтой уже существует");
+  } else {
+    userSignup.classList.remove("hide");
+  }
+  o.preventDefault();
 });
 
-const masterForm = document.getElementById("add-master");
-masterForm.addEventListener("submit", (event) => {
-  var notifications = document.getElementById("notification2");
-  if (notifications.checked) {
-    notifications.value = true;
-  } else {
-    notifications.value = false;
+window.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    userSignup.classList.add("hide");
   }
-  event.preventDefault();
-  db.collection("masters").add({
-    full_name: masterForm.full_name.value,
-    email: masterForm.email.value,
-    salon_name: masterForm.salonname.value,
-    phone_number: masterForm.phone_number.value,
-    password: masterForm.password.value,
-    notifications: masterForm.notifications.value
-  });
-  masterForm.reset();
 });
+userSignup.addEventListener("click", (event) => {
+  const target = event.target;
+
+  if (target.classList.contains("modal-close") || target === userSignup) {
+    userSignup.classList.add("hide");
+  }
+});
+
+window.onload = function () {
+  render();
+};
+function render() {
+  window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
+    "recaptcha-container"
+  );
+  recaptchaVerifier.render();
+}
+function phoneAuth() {
+  //get the number
+  var number = document.getElementById("phone_number").value;
+  //phone number authentication function of firebase
+  //it takes two parameter first one is number,,,second one is recaptcha
+  firebase
+    .auth()
+    .signInWithPhoneNumber(number, window.recaptchaVerifier)
+    .then(function (confirmationResult) {
+      //s is in lowercase
+      window.confirmationResult = confirmationResult;
+      coderesult = confirmationResult;
+      console.log(coderesult);
+      alert("Код отправлен");
+    })
+    .catch(function (error) {
+      alert(error.message);
+    });
+}
+
+function codeverify() {
+  var code = document.getElementById("verificationCode").value;
+  coderesult
+    .confirm(code)
+    .then(function (result) {
+      var user = result.user;
+      console.log(user);
+      if (masterForm.classList.contains("hide")) {
+        alert("Вы зарегистрировались");
+        var notifications = document.getElementById("notification1");
+        if (notifications.checked) {
+          notifications.value = true;
+        } else {
+          notifications.value = false;
+        }
+        database.collection("salons").add({
+          full_name: form.full_name.value,
+          email: form.email.value,
+          salon_name: form.salonname.value,
+          phone_number: phoneform.phone_number.value,
+          notifications: notifications.value,
+          password: form.password.value,
+        });
+        userSignup.classList.add("hide");
+      } else {
+        alert("Вы зарегистрировались");
+        var notification = document.getElementById("notification2");
+        if (notification.checked) {
+          notification.value = true;
+        } else {
+          notification.value = false;
+        }
+        database.collection("masters").add({
+          full_name: masterForm.full_name.value,
+          email: masterForm.email.value,
+          salon_name: masterForm.salonname.value,
+          phone_number: phoneform.phone_number.value,
+          notifications: notification.value,
+          password: masterForm.password.value,
+        });
+        userSignup.classList.add("hide");
+      }
+    })
+    .catch(function (error) {
+      alert(error.message);
+    });
+}
